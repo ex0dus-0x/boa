be2474290b93daf1377934b7cdbb9bb7615d0249
ex0dus-0x <ex0dus@codemuch.tech>
2020-07-31 16:57:45 -0400

    Make decompiler work

--- boa/decompile.py
+++ boa/decompile.py
@@ -9,10 +9,29 @@ import os
 import sys
 import ntpath
 import pkgutil
+import subprocess
 
 import requests
+import stdlib_list
 import uncompyle6
 import decompyle3
+
+# other modules that we don't care about
+MOD_DONT_CARE = [
+    "pkg_resources",
+
+    # Windows-specific Python runtime libraries
+    "commctr",
+    "netbios",
+    "pythoncom",
+    "pywin",
+    "pywintypes",
+    "win32com",
+    "win32con",
+    "win32evtlogutil",
+    "win32traceutil",
+    "winerror",
+]
 
 
 class BoaDecompiler(object):
@@ -21,64 +40,104 @@ class BoaDecompiler(object):
     modules for source recovery when given bytecode. Implements rudimentary
     bytecode patching if decompilers are unable to parse the input further.
     """
-    def __init__(self, workspace, pyver, decomp_all=False):
+    def __init__(self, workspace, pyver, paths, decomp_all=False):
+
+        # reuse workspace for writing source
         self.workspace = workspace
 
-        # used to identify the first decompiler to be used
-        #   Python 3.0 - 3.8: decompyle3
+        # instantiate list of all standard library modules
+        self.stdlib = stdlib_list.stdlib_list("3.8")
+
+        # instantiate a local dataset of top PyPI packages
+        self.packages = BoaDecompiler.iter_packages()
+
+        # Main decompiler used: uncompyle
+        # However, in case uncompyle6 fails decompilation, we fallback:
+        #   Python 3.x: decompyle3
         #   Python 2.7.x: py2
         self.pyver = pyver
+        self.decompiler = "uncompyle6"
 
-        # if set, all bytecode files will be indiscriminantly decompiled
-        self.decomp_all = decomp_all
+        # used to cache deps already tested to ignore
+        self.cached_ignore_deps = set()
 
-        # stores a list of paths to actually decompile
-        self.paths = list()
+        # stores a set of all external and internal dependencies
+        self.total_deps = set([
+            ntpath.basename(path).split(".")[0]
+            for path in paths
+        ])
 
-        # total number of modules
-        self.total_deps = set()
+        # create a mapping with all deps as keys with vals as empty list storing paths
+        self.dep_mapping = {
+            dep: list()
+            for dep in self.total_deps
+        }
+
+        # reiterate paths and populate self.dep_mapping
+        for path in paths:
+            base = ntpath.basename(path).split(".")[0]
+
+            # check if dep is found as key
+            if base in self.dep_mapping.keys():
+
+                # check before decompiling
+                if not decomp_all:
+
+                    # check if appropriate to decompile, otherwise delete
+                    if self._check_if_decompile(base):
+                        self.dep_mapping[base] += [path]
+                    else:
+                        del self.dep_mapping[base]
+
+                # otherwise add all indiscriminantly
+                else:
+                    self.dep_mapping[base] += [path]
 
 
-    def _check_if_decompile(self, name) -> bool:
+    @staticmethod
+    def iter_packages():
+        """
+        Helper function to Get dataset of top PyPI packages to check deps against
+        """
+        res = requests.get("https://hugovk.github.io/top-pypi-packages/top-pypi-packages-365-days.json")
+        if res.status_code != 200:
+            raise Exception("Cannot get packages")
+
+        # convert to dictionary and get all package names
+        pkgs_dict = dict(res.json())
+        return [pkg["project"] for pkg in pkgs_dict["rows"]]
+
+
+
+    def _check_if_decompile(self, dep) -> bool:
         """
         To save time and space, ignore decompiling known dependencies and add them to a set.
-        May produce false positives, but hope is to reduce overtime decompiling unnecessary
+        May produce false positives, but hope is to reduce overhead decompiling unnecessary
         dependencies that are open-sourced.
         """
 
-        # cleanup dep path: parse out filename, and strip extension
-        dep = os.path.splitext(ntpath.basename(name))[0]
+        # check if this is has been cached, and return quickly
+        if dep in list(self.cached_ignore_deps):
+            return False
 
         # check if dependency exists as a built-in module
-        if any([val in dep for val in list(sys.builtin_module_names)]):
-            return
+        if dep in list(sys.builtin_module_names):
+            self.cached_ignore_deps.update(dep)
+            return False
 
         # check if dependency exists as a standard library module
-        stdlib = [
-            mod for _, mod, pkg in list(pkgutil.iter_modules())
-            if pkg is False
-        ]
-        if any([val in dep for val in stdlib]):
-            return
+        if dep in self.stdlib:
+            self.cached_ignore_deps.update(dep)
+            return False
 
-        # if dependency has multiple submodules, chances are it is an existing PyPI package
-        # parse out module name, and check to see if version exists upstream, and ignore if so
-        if "." in dep:
-            modname = dep.split(".")[0]
+        # check if package is popular on PyPI
+        if dep in self.packages:
+            self.cached_ignore_deps.update(dep)
+            return False
 
-            # don't repeat again if already parsed once
-            if not modname in self.deps:
-                resp = requests.get("https://pypi.org/pypi/{}/json".format(modname))
-
-                # if found, add to set and return
-                if resp.status_code == 200:
-                    self.deps.update(modname)
-                    return False
-            else:
-                return False
-
-        # last check: remove any other modules already parsed
-        if any([val in dep for val in list(self.deps)]):
+        # check if module is something we don't care about"
+        if dep in MOD_DONT_CARE:
+            self.cached_ignore_deps.update(dep)
             return False
 
         # there may be smaller deps that end up as false positives, since we can't make a distinction
@@ -86,12 +145,22 @@ class BoaDecompiler(object):
         return True
 
 
-    def decompile(self, paths):
+    def decompile_all(self):
         """
-        Given a list of paths to a .pyc bytecode file, check if decompilation is necessary, ...
+        Given all the stored paths of relevant bytecode files, decompile all of them into the workspace directory.
         """
-        self.total_deps = set([
-            ntpath.basename(path).split(".")[0]
-            for path in paths
-        ])
+        for _, paths in self.dep_mapping.items():
+            for path in paths:
 
+                # get abspath to bytecode path and call decompiler
+                decomp_path = os.path.join(self.workspace, "unpacked", path)
+                proc = subprocess.Popen([self.decompiler, decomp_path], stdout=subprocess.PIPE)
+                output = proc.stdout.read()
+
+                # write result to path
+                filename = ntpath.basename(path).replace("pyc", "py")
+                store_path = os.path.join(self.workspace, "recovered", filename)
+                with open(store_path, "wb") as fd:
+                    fd.write(output)
+
+        print("Done")
